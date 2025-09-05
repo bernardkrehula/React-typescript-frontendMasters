cbe62545283667e675057b8179045b0824c129f9
Bernard Krehula <krehula.bernard456@gmail.com>
2025-09-05 19:45:40 +0200

    control file change from jsx to tsx

--- name-badges-main/src/components/controls.tsx
+++ name-badges-main/src/components/controls.tsx
@@ -1,6 +1,10 @@
-import PropTypes from 'prop-types';
 
-const ControlPanel = ({ name, onChange }) => {
+type ControlPanelProps = {
+  name: string;
+  onChange: any;
+}
+
+const ControlPanel = ({ name, onChange }: ControlPanelProps) => {
   return (
     <form
       className="flex flex-row gap-4 text-white bg-primary-10"
@@ -18,11 +22,6 @@ const ControlPanel = ({ name, onChange }) => {
       </div>
     </form>
   );
-};
-
-ControlPanel.propTypes = {
-  name: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 export default ControlPanel;
